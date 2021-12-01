5b9a735c8f6b7302c0f23671ee3d9a73db81933d
Sang sakarin <sang_sakarin@outlook.com>
2021-12-01 15:38:48 +0700

    feat: add database postgres setup #2

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,5 +3,9 @@ package config
 var Cfg config
 
 type config struct {
-	ENV string `env:"ENV"`
+	ENV              string `env:"ENV"`
+	DatabaseHost     string `env:"DATABASE_HOST"`
+	DatabaseUser     string `env:"DATABASE_USER"`
+	DatabasePassword string `env:"DATABASE_PASSWORD"`
+	DatabaseName     string `env:"DATABASE_NAME"`
 }
